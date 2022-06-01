b1813cdd20d55c81ee98a7453131aac4f6b5fcdd
Vishnu Sathwik Rebally <87108196+vishnusathwik@users.noreply.github.com>
2022-06-01 11:04:40 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,12 @@
+const mongoose  = require('mongoose')
+const mongoURI = "mongodb://0.0.0.0:27017/inotebook"
+
+const connectToMongo = () =>{
+    mongoose.connect(mongoURI , () =>{
+        console.log('connected to mongo successfully');
+    })
+
+}
+
+module.exports = connectToMongo;
+//module.exports = mongoose.model('user', UserSchema); 
